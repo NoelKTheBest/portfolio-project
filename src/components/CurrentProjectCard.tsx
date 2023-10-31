7817227f31a55904c45806e76e9fc971db49710c
NKDevArt <nk.toptier.0@gmail.com>
2023-10-31 11:23:20 -0700

    Update CurrentProjectCard.tsx

--- src/components/CurrentProjectCard.tsx
+++ src/components/CurrentProjectCard.tsx
@@ -1,23 +1,25 @@
-import { Card, CardBody } from "@chakra-ui/react";
+import { Box, Card, CardBody } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
 import projectImage from "../assets/trigger-collider.png";
 import { Flex } from "@chakra-ui/react";
 
 const CurrentProjectCard = () => {
   return (
-    <Flex justifyContent="space-around">
-      <Card width={300} position="relative">
-        <CardBody>
-          <Image
-            src={projectImage}
-            width={250}
-            alt="Current Project Snapshot"
-            borderRadius="lg"
-            marginX="auto"
-          ></Image>
-        </CardBody>
-      </Card>
-    </Flex>
+    <Box bgColor="#46a34b">
+      <Flex justifyContent="space-around">
+        <Card width={300} position="relative">
+          <CardBody>
+            <Image
+              src={projectImage}
+              width={250}
+              alt="Current Project Snapshot"
+              borderRadius="lg"
+              marginX="auto"
+            ></Image>
+          </CardBody>
+        </Card>
+      </Flex>
+    </Box>
   );
 };
 
